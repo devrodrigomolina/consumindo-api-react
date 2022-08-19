f02daad08d15e4219ad7987c1750a6a990752265
PedroAlmeidaFetchly <107511028+PedroAlmeidaFetchly@users.noreply.github.com>
2022-08-18 21:35:09 -0300

    renderizando os dados

--- src/componentes/PegaAlbum.jsx
+++ src/componentes/PegaAlbum.jsx
@@ -1,18 +1,31 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
 const PegaComponente = () => {
+  const [produtos, setProdutos] = useState([]);
+
   const pegaDados = async () => {
     console.log("pega dados");
     const res = await fetch("https://ranekapi.origamid.dev/json/api/produto/");
     const dados = await res.json();
+    setProdutos(dados);
     console.log(dados);
   };
 
   useEffect(() => {
     pegaDados();
   }, []);
+  console.log(produtos === null);
 
-  return <h1>Pega Componente</h1>;
+  return (
+    <>
+      <h1>Pega Componente</h1>
+      {produtos.map((produto) => (
+        <div key={produto.id}>
+          <p>{produto.descricao}</p>
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default PegaComponente;
