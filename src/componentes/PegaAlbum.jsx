75bf719623edd4dc421e63abfb4e5caad96bc06e
PedroAlmeidaFetchly <107511028+PedroAlmeidaFetchly@users.noreply.github.com>
2022-08-18 21:24:36 -0300

    dados tratados

--- src/componentes/PegaAlbum.jsx
+++ src/componentes/PegaAlbum.jsx
@@ -3,6 +3,9 @@ import { useEffect } from "react";
 const PegaComponente = () => {
   const pegaDados = async () => {
     console.log("pega dados");
+    const res = await fetch("https://ranekapi.origamid.dev/json/api/produto/");
+    const dados = await res.json();
+    console.log(dados);
   };
 
   useEffect(() => {
